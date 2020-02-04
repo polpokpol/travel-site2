11abe417122337cb403c6b3a4294561a16ceab47
Ringo <polpokpol2@gmail.com>
2020-02-05 01:40:41 +0800

    Completed the reveal on scroll behavior.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,12 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu.js'
+import RevealOnScroll from './modules/RevealOnScroll.js';
+
 
 let mobileMenu = new MobileMenu();
+// let revealOnScroll = new RevealOnScroll();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 
 // alert("Im sick and also this is a test!" + __dirname);
 
