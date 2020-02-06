db744e5dad83aefadd27bfc2345341c08344a836
Ringo <polpokpol2@gmail.com>
2020-02-06 22:54:25 +0800

    Completed sticky header function.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu.js'
 import RevealOnScroll from './modules/RevealOnScroll.js';
+import StickyHeader from './modules/StickyHeader.js';
 
 
+let stickyHeader = new StickyHeader();
 let mobileMenu = new MobileMenu();
 // let revealOnScroll = new RevealOnScroll();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
